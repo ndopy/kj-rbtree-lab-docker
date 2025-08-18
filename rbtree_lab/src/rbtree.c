2993e686fad5518c29da6a25c7853e4017538c8d
hugo <vvhooping@gmail.com>
2025-08-18 20:28:23 +0900

    feat: rbtree_erase 구현

--- rbtree_lab/src/rbtree.c
+++ rbtree_lab/src/rbtree.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  if (p == NULL) {
+  rbtree *tree = (rbtree *)calloc(1, sizeof(rbtree));
+  if (tree == NULL) {
     return NULL;
   }
 
   // nil 노드(=센티넬) 을 위한 메모리 할당 및 초기화
   node_t *nil = (node_t *)calloc(1, sizeof(node_t));
   if (nil == NULL) {
-    free(p);
+    free(tree);
     return NULL;
   }
 
@@ -19,10 +19,10 @@ rbtree *new_rbtree(void) {
   nil->color = RBTREE_BLACK;
 
   // RB Tree 구조체 초기화
-  p->nil = nil;
-  p->root = nil;  // 처음에는 root 가 nil 노드를 가리켜야 함. (=빈 트리)
+  tree->nil = nil;
+  tree->root = nil;  // 처음에는 root 가 nil 노드를 가리켜야 함. (=빈 트리)
 
-  return p;
+  return tree;
 }
 
 void delete_nodes_recursively(node_t *node, node_t *nil) {
@@ -49,7 +49,6 @@ void delete_rbtree(rbtree *t) {
   free(t);
 }
 
-
 void left_rotate(rbtree *t, node_t *x) {
   node_t *y = x->right;
 
@@ -73,7 +72,6 @@ void left_rotate(rbtree *t, node_t *x) {
   y->left = x;          // 부모 입장
   x->parent = y;        // 자식 입장
 }
-
 
 void right_rotate(rbtree *t, node_t *x) {
   node_t *y = x->left;
@@ -247,8 +245,141 @@ node_t *rbtree_max(const rbtree *t) {
   return current;
 }
 
+node_t* get_successor(rbtree *t, node_t *p) {
+  node_t *current = p->right;   // 오른쪽으로 한 칸 이동
+
+  while (current->left != t->nil) { // 왼쪽 자녀가 없을 때까지 계속 이동
+    current = current->left;
+  }
+
+  return current;
+}
+
+/**
+ * 특정 노드 u를 다른 노드 v로 교체할 때 부모 자식 간의 포인터 관계를 정리하는 함수
+ * @param t RB Tree
+ * @param u 삭제될 노드
+ * @param v 삭제될 노드를 대체할 노드
+ */
+void rbtree_transplant(rbtree *t, node_t *u, node_t *v) {
+  // 1. u의 부모가 v를 가리키도록 설정 (부모 입장)
+  if (u->parent == t->nil) {          // u가 root 노드일 경우 -> v가 새로운 root가 된다.
+    t->root = v;
+  } else if (u == u->parent->left) {  // u가 왼쪽 자식일 경우 -> u의 부모의 왼쪽 자식을 v로 바꾼다.
+    u->parent->left = v;
+  } else {                            // u가 오른쪽 자식일 경우 -> u의 부모의 오른쪽 자식을 v로 바꾼다.
+    u->parent->right = v;
+  }
+
+  // 2. v가 u의 부모를 새로운 부모로 가리키도록 설정 (자식 입장)
+  v->parent = u->parent;
+}
+
+void rbtree_erase_fixup(rbtree *t, node_t *x) {
+  node_t *sibling;
+
+  while (x != t->root && x->color == RBTREE_BLACK) {
+    // Case: x가 왼쪽 자식인 경우
+    if (x == x->parent->left) {
+      sibling = x->parent->right;
+
+      // Case 1: 형제가 RED인 경우
+      if (sibling->color == RBTREE_RED) {
+        sibling->color = RBTREE_BLACK;  // 형제는 BLACK으로
+        x->parent->color = RBTREE_RED;  // 부모는 RED로
+        left_rotate(t, x->parent);      // 부모를 기준으로 좌회전
+        sibling = x->parent->right;     // 새로운 형제로 갱신
+      }
+
+      // 이제 형제는 반드시 BLACK이다.
+      // Case 2: 형제의 두 자식도 BLACK인 경우
+      if (sibling->left->color == RBTREE_BLACK && sibling->right->color == RBTREE_BLACK) {
+        sibling->color = RBTREE_RED;   // 형제를 RED로 만들고
+        x = x->parent;                 // extra black 을 부모에게 넘긴다.
+      } else if (sibling->right->color == RBTREE_BLACK) {
+        // Case 3: 형제는 BLACK, 형제의 왼쪽 자식은 RED, 오른쪽 자식은 BLACK
+        sibling->left->color = RBTREE_BLACK;  // 형제의 왼쪽 자식을 BLACK으로
+        sibling->color = RBTREE_RED;          // 형제는 RED로
+        right_rotate(t, sibling);             // 형제를 기준으로 우회전
+        sibling = x->parent->right;           // 새로운 형제로 갱신
+      } else {
+        // Case 4: 형제는 BLACK, 형제의 왼쪽 자식은 BLACK, 오른쪽 자식은 RED
+        sibling->color = x->parent->color;    // 형제는 부모의 색을 물려받음
+        sibling->right->color = RBTREE_BLACK; // 형제의 오른쪽 자식을 BLACK으로
+        x->parent->color = RBTREE_BLACK;      // 부모는 BLACK 으로
+        left_rotate(t, x->parent);            // 부모를 기준으로 좌회전
+        break;                                // 루프 종료
+      }
+    } else {
+      // Case: x가 오른쪽 자식일 경우
+      sibling = x->parent->left;
+
+      // Case 1: 형제가 RED인 경우
+      if (sibling->color == RBTREE_RED) {
+        sibling->color = RBTREE_BLACK;    // 형제는 BLACK으로
+        x->parent->color = RBTREE_RED;    // 부모는 RED로
+        right_rotate(t, x->parent);       // 부모를 기준으로 우회전
+        sibling = x->parent->left;        // 새로운 형제로 갱신
+      }
+
+      // 이제 형제는 반드시 BLACK 이다.
+      // Case 2: 형제의 두 자식도 모두 BLACK인 경우
+      if (sibling->left->color == RBTREE_BLACK && sibling->right->color == RBTREE_BLACK) {
+        sibling->color = RBTREE_RED;   // 형제를 RED로 만들고
+        x = x->parent;                 // extra black을 부모에게 넘긴다.
+      } else if (sibling->left->color == RBTREE_BLACK) {
+        // Case 3: 형제는 BLACK, 형제의 왼쪽 자식은 BLACK, 오른쪽 자식은 RED
+        sibling->right->color = RBTREE_BLACK;  // 형제의 오른쪽 자식을 BLACK으로
+        sibling->color = RBTREE_RED;           // 형제는 RED로
+        left_rotate(t, sibling);               // 형제를 기준으로 좌회전
+        sibling = x->parent->left;             // 새로운 형제로 갱신
+      } else {
+        // Case 4: 형제는 BLACK, 형제의 왼쪽 자식은 RED, 오른쪽 자식은 BLACK
+        sibling->color = x->parent->color;     // 형제는 부모의 색을 물려받음
+        sibling->left->color = RBTREE_BLACK;   // 형제의 왼쪽 자식을 BLACK으로
+        x->parent->color = RBTREE_BLACK;       // 부모는 BLACK으로
+        right_rotate(t, x->parent);            // 부모를 기준으로 우회전
+        break;                                 // 루프 종료
+      }
+    }
+  } // end while
+
+  // red-and-black 노드 처리 및 루트는 BLACK이어야 한다는 속성 처리
+  x->color = RBTREE_BLACK;
+}
+
 int rbtree_erase(rbtree *t, node_t *p) {
-  // TODO: implement erase
+  // 1. 물리적으로 삭제될 노드와 그 색상을 결정
+  node_t *y = p;
+  color_t y_original_color = y->color;
+  char has_two_children = (p->left != t->nil && p->right != t->nil);
+
+  if (has_two_children) {                     // 삭제될 노드의 자녀가 2개 : 삭제되는 노드의 Successor 의 색
+    y = get_successor(t, p);                  // successor 찾기
+    y_original_color = y->color;              // 삭제되는 색상 = successor 의 색상
+  }
+
+  // 2. y를 대체할 자식 노드(x) 찾기
+  // x는 y의 자식들 중 nil이 아닌 노드이거나 nil일 수 있다.
+  node_t *x = (y->left != t->nil) ? y->left : y->right;
+
+  // 3. y를 트리에서 떼어내기 (transplant)
+  // y의 부모와 x를 연결하기
+  rbtree_transplant(t, y, x);
+
+  // 4. 만약 p의 자녀가 2개였다면 p의 key 값을 y(=successor)의 값으로 업데이트
+  if (y != p) {
+    p->key = y->key;
+  }
+
+  // 5. BLACK 노드가 삭제되었다면 fixup 호출
+  if (y_original_color == RBTREE_BLACK) {
+    rbtree_erase_fixup(t, x);
+  }
+
+  // 삭제한 노드의 메모리 해제
+  free(y);
+
   return 0;
 }
 
